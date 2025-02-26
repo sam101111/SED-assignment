aa0c5e58288a11fe2b1d0fb05117f6d6507db876
Sam <sam.q.ldn2022@proton.me>
2025-02-26 15:22:19 -0800

    fix: NoneType fix

--- app/config.py
+++ app/config.py
@@ -8,6 +8,9 @@ if os.environ.get("RENDER") is None:
 # Allows for one central place to define and edit env varibles
 class Config:
     bool_map = {"true": True, "false": False}
-
-    SECURE_COOKIES = bool_map.get(os.environ.get("SECURE_COOKIES").strip().lower(), False)
+    # If its not set it will default to false, allowing the pipeline to run
+    if os.environ.get("SECURE_COOKIES") is None:
+        SECURE_COOKIES = False
+    else:
+        SECURE_COOKIES = bool_map.get(os.environ.get("SECURE_COOKIES").strip().lower(), False)
 config = Config()
